36694318b216bbca563adcc96d034c717ef0f19c
Chris Smith <john.chris.smith@gmail.com>
2024-01-12 07:53:05 -0600

    Changed jpg to png, tweaked prompts

--- debater.py
+++ debater.py
@@ -18,7 +18,7 @@ dotenv.load_dotenv()
 
 client = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
 
-image_size = "1792x1024"
+image_size = "1024x1024"
 
 class DebateParticipant(ABC):
     def __init__(self, model, name, role, topic, temperature="1.0"):
@@ -29,7 +29,7 @@ class DebateParticipant(ABC):
         self.temperature = temperature
 
     def get_system_message(self):
-        return f"You are {self.name}. You are debating as a {self.role} of {self.topic}. Your response should be a json object with the keys 'speaker', 'content', and 'image_description'. The speaker should be your name. The content should be your response. The image_description should be a description of an image to accompany your response. The image will be displayed on screen while your response is being read aloud. Avoid anything explicit or inappropriate. Avoid anything offensive. Avoid directly mentioning anything that is copyrighted."
+        return f"You are {self.name}. You are debating as a {self.role} of {self.topic}. Your response should be a json object with the keys 'speaker', 'content', and 'image_description'. The speaker should be your name. The content should be your response. The image_description should be a description of an image to accompany your statement. Avoid using images of the debate setting or the debaters. Avoid using images of celebrities or public figures. Our text to image system uses a content filter, so avoid anything inappropriate. Avoid anything offensive. Avoid directly mentioning anything that is copyrighted."
 
     def get_response(self, debate_messages, instruction=""):
         response = client.chat.completions.create(
@@ -42,7 +42,7 @@ class DebateParticipant(ABC):
                 },
                 {
                     "role": "user",
-                    "content": json.dumps(debate_messages),
+                    "content": json.dumps(debate_messages, ensure_ascii=False),
                 },
                 {
                     "role": "user",
@@ -80,75 +80,35 @@ class DebateManager:
         for round_info in self.debate_config["rounds"]:
             print(f"\n--- {round_info['name']} ---")
             for step_info in round_info["steps"]:
-                role, instruction = step_info if isinstance(step_info, tuple) else (step_info, "")
+                if isinstance(step_info, list) and len(step_info) == 2:
+                    role, instruction = step_info
+                else:
+                    role = step_info
+                    instruction = ""
                 participant = self.participants[role.lower()]
                 response = participant.get_response(self.debate_messages, instruction)
-                print(f"{participant.name}: {response['content']}\n")
-
+                print(f"{response['speaker']}: {response['content']}\n\nimage_description: {response['image_description']}\n\n\n")
         return self.debate_messages
 
+def load_debate_config(file_path):
+    with open(file_path, "r") as file:
+        debate_config = json.load(file)
+    return debate_config
+
+debate_config = load_debate_config("debate_config.json")
 
 # input the debate topic
 
 topic = input("What is the topic of the debate? ")
 
-debate_config = {
-    "topic": topic,
-    "participants": [
-        {
-            "name": "Moderator",
-            "role": "moderator",
-            "model": "gpt-4-1106-preview",
-            "temperature": "0.7",
-        },
-        {
-            "name": "Alex",
-            "role": "proponent",
-            "model": "gpt-4-1106-preview",
-            "temperature": "1.1",
-        },
-        {
-            "name": "Jordan",
-            "role": "opponent",
-            "model": "gpt-4-1106-preview",
-            "temperature": "1.1",
-        },
-    ],
-"rounds": [
-    {
-        "name": "Opening Constructive Statements",
-        "steps": [("moderator", "The proponent is Alex and the opponent is Jordan. Each debater should give a 5 minute constructive speech"), "proponent", "opponent"]},
-    {
-        "name": "Cross Examination",
-        "steps": [
-            ("moderator", "Introduce the cross-examination round. The opponent will go first."),
-            ("opponent", "Question to Proponent"),
-            ("proponent", "Answer to Opponent's Question"),
-            ("moderator", "Ask Proponent for their question"),
-            ("proponent", "Question to Opponent"),
-            ("opponent", "Answer to Proponent's Question")
-        ]
-    },
-    {"name": "Closing Arguments", "steps": [
-        ("moderator", "Introduce the closing arguments round"),
-        "proponent",
-        "opponent",
-        ("moderator", "conclude the debate. Thank the participants and the audience. Ask viewers to comment, like, and subscribe.")
-        ]},
-],
 
-}
+debate_config["topic"] = topic
 
-topic = debate_config["topic"]
 # Instantiate DebateManager
 debate_manager = DebateManager(debate_config)
 
 # Conduct the debate
 debate_messages = debate_manager.conduct_debate()
-
-# Print the debate messages
-for msg in debate_messages:
-    print(f"{msg['speaker']}: {msg['content']}")
 
 # create a unique name for the debate
 topic_slug = slugify.slugify(topic)
@@ -169,7 +129,7 @@ transcript_file_path = os.path.join(debate_folder, "transcript.json")
 
 # Save the transcript
 with open(transcript_file_path, "w") as file:
-    json.dump(debate_messages, file, indent=4)
+    json.dump(debate_messages, file, indent=4, ensure_ascii=False)
 
 print(f"Transcript saved to {transcript_file_path}")
 
@@ -210,7 +170,7 @@ for i, message in enumerate(debate_messages):
             model="dall-e-3",
             prompt=f"{image_description}",
             size=image_size,
-            quality="hd",
+            quality="standard",
             style="vivid",
             n=1,
         )
@@ -219,7 +179,7 @@ for i, message in enumerate(debate_messages):
         continue
     image_url = response.data[0].url
     image = requests.get(image_url)
-    with open(f"{debate_folder}/{i:03}.jpg", "wb") as f:
+    with open(f"{debate_folder}/{i:03}.png", "wb") as f:
         f.write(image.content)
 
 # start creating a video from the images and audio files
@@ -227,39 +187,39 @@ for i, message in enumerate(debate_messages):
 
 directory = debate_folder
 
-# get all jpg files in the directory
+# get all png files in the directory
 
-jpg_files = list(pathlib.Path(directory).glob("*.jpg"))
+png_files = list(pathlib.Path(directory).glob("*.png"))
 
 # get all mp3 files in the directory
 
 mp3_files = list(pathlib.Path(directory).glob("*.mp3"))
 
-print(f"{mp3_files}\n\n{jpg_files}")
+print(f"{mp3_files}\n\n{png_files}")
 
 # sort the files by name
 
-jpg_files.sort()
+png_files.sort()
 mp3_files.sort()
 
 # create a list of clips
 
 clips = []
 
-# for example, 0.jpg and 0.mp3 should be combined into a video clip.
+# for example, 0.png and 0.mp3 should be combined into a video clip.
 # the video clip should match the duration of the mp3 file.
 
-for i in range(len(jpg_files)):
-    jpg_file = str(jpg_files[i])
+for i in range(len(png_files)):
+    png_file = str(png_files[i])
     mp3_file = str(mp3_files[i])
-    print(f"Processing {jpg_file} and {mp3_file}")
-    jpg_clip = ImageClip(jpg_file)
+    print(f"Processing {png_file} and {mp3_file}")
+    png_clip = ImageClip(png_file)
     mp3_clip = AudioFileClip(mp3_file)
-    jpg_clip = jpg_clip.set_duration(mp3_clip.duration)
-    jpg_clip = jpg_clip.set_audio(
+    png_clip = png_clip.set_duration(mp3_clip.duration)
+    png_clip = png_clip.set_audio(
         mp3_clip
-    )  # Set the audio of the jpg_clip to be the mp3_clip
-    clips.append(jpg_clip)
+    )  # Set the audio of the png_clip to be the mp3_clip
+    clips.append(png_clip)
 
 # concatenate the clips
 final_clip = concatenate_videoclips(clips)
--- vidgen.py
+++ vidgen.py
@@ -4,41 +4,41 @@ import sys
 import pathlib
 
 # input arguments
-# user will specify a folder which contains jpg and mp3 files. these will be combined into a video.
+# user will specify a folder which contains png and mp3 files. these will be combined into a video.
 
 directory = sys.argv[1]
 
-# get all jpg files in the directory
+# get all png files in the directory
 
-jpg_files = list(pathlib.Path(directory).glob("*.jpg"))
+png_files = list(pathlib.Path(directory).glob("*.png"))
 
 # get all mp3 files in the directory
 
 mp3_files = list(pathlib.Path(directory).glob("*.mp3"))
 
-print (f"{mp3_files}\n\n{jpg_files}")
+print (f"{mp3_files}\n\n{png_files}")
 
 # sort the files by name
 
-jpg_files.sort()
+png_files.sort()
 mp3_files.sort()
 
 # create a list of clips
 
 clips = []
 
-# for example, 0.jpg and 0.mp3 should be combined into a video clip.
+# for example, 0.png and 0.mp3 should be combined into a video clip.
 # the video clip should match the duration of the mp3 file.
 
-for i in range(len(jpg_files)):
-    jpg_file = str(jpg_files[i])
+for i in range(len(png_files)):
+    png_file = str(png_files[i])
     mp3_file = str(mp3_files[i])
-    print(f"Processing {jpg_file} and {mp3_file}")
-    jpg_clip = ImageClip(jpg_file)
+    print(f"Processing {png_file} and {mp3_file}")
+    png_clip = ImageClip(png_file)
     mp3_clip = AudioFileClip(mp3_file)
-    jpg_clip = jpg_clip.set_duration(mp3_clip.duration)
-    jpg_clip = jpg_clip.set_audio(mp3_clip)  # Set the audio of the jpg_clip to be the mp3_clip
-    clips.append(jpg_clip)
+    png_clip = png_clip.set_duration(mp3_clip.duration)
+    png_clip = png_clip.set_audio(mp3_clip)  # Set the audio of the png_clip to be the mp3_clip
+    clips.append(png_clip)
 
 # concatenate the clips
 final_clip = concatenate_videoclips(clips)
